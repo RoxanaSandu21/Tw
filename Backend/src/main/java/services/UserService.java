caa6d8d8264a9372736ed6ddf911291b851070e7
DMG0411 <m2bobko@gmail.com>
2023-06-18 22:46:19 +0300

    WishList functionalities of User.

--- Backend/src/main/java/services/UserService.java
+++ Backend/src/main/java/services/UserService.java
@@ -2,11 +2,14 @@ package services;
 
 import dataprovider.Data;
 import models.Flower;
+import models.FlowerFavoriteType;
 import models.User;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.*;
 
 public class UserService {
 
@@ -82,5 +85,70 @@ public class UserService {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void removeFlowerFromWishList (String flowerName, String email) {
+        String query = "DELETE FROM favorites where email = ? AND flower_name = ?";
+
+        try (PreparedStatement statement = Data.getInstance().getConnection().prepareStatement(query)) {
+            statement.setString(1, email);
+            statement.setString(2, flowerName);
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void addFlowerOnWishList (String flowerName, String email) {
+        String query = "INSERT INTO favorites VALUES (?, ?, ?)";
+
+        try (PreparedStatement statement = Data.getInstance().getConnection().prepareStatement(query)) {
+            statement.setString(1, email);
+            statement.setString(2, flowerName.toLowerCase());
+            statement.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static boolean isFlowerAddedToWishList (String flowerName, String email) {
+        String query = "SELECT email, flower_name FROM favorites WHERE email = ? and flower_name = ?";
+
+        try (PreparedStatement statement = Data.getInstance().getConnection().prepareStatement(query)) {
+            statement.setString(1, email);
+            statement.setString(2, flowerName.toLowerCase());
+
+            ResultSet resultSet = statement.executeQuery();
+
+            return resultSet.next();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<FlowerFavoriteType> getFavoriteFlowersForUser (String email) {
+        String query = "SELECT flower_name FROM favorites WHERE email =?";
+        List<FlowerFavoriteType> favoriteFlowers;
+
+        try (PreparedStatement statement = Data.getInstance().getConnection().prepareStatement(query)) {
+            statement.setString(1, email);
+
+            ResultSet resultSet = statement.executeQuery();
+            favoriteFlowers = new LinkedList<>();
+
+            while (resultSet.next()) {
+                FlowerFavoriteType flowerFavorite = new FlowerFavoriteType();
+                flowerFavorite.setFlowerName(resultSet.getString("flower_name"));
+                favoriteFlowers.add(flowerFavorite);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return favoriteFlowers;
     }
 }
